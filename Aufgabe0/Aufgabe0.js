1a9ccb92faf4351c87e13c10dad1dca008661e5f
verenagirrbach <verenagirrbach@verenas-macbook.fritz.box>
2018-10-08 13:40:27 +0200

    js wird erstellt

---
+++ Aufgabe0/Aufgabe0.js
@@ -0,0 +1,8 @@
+function halloDu() {
+    var person = prompt("Wie heißt du?");
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+            "Hey" + person;
+    }
+}
+//# sourceMappingURL=Aufgabe0.js.map
