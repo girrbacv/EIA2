5a44b18f871acdc24d52ba59605db8df400e2b6d
verenagirrbach <verenagirrbach@verenas-macbook.fritz.box>
2018-11-25 15:50:43 +0100

    Assoziative Arrays

--- Aufgabe4/Christmastree/Haendler.js
+++ Aufgabe4/Christmastree/Haendler.js
@@ -1,8 +1,34 @@
-var christmastree;
-(function (christmastree) {
-    christmastree.trees = [{ name: "Nordmanntanne", img: "imgs/01.jpg", price: 34.95 }, { name: "Rotfichte", img: "imgs/02.jpg", price: 29.90 }, { name: "Blaufichte", img: "imgs/03.jpg", price: 29.90 }, { name: "Edeltanne", img: "imgs/04.jpg", price: 39.90 }];
-    christmastree.ornaments = [{ name: "Glaskugel rot", img: "imgs/WK1.jpg", price: 1.54 }, { name: "Glaskugel grün", img: "imgs/WK2.jpg", price: 1.54 }, { name: "Glaskugel creme", img: "imgs/WK3.jpg", price: 1.54 }, { name: "Candycane", img: "imgs/WK4.jpg", price: 3.99 }, { name: "Glaskugel,rot,Herz", img: "imgs/WK5.jpg", price: 3.99 }, { name: "Glaskugel,candycane,grün", img: "imgs/WK6", price: 3.99 }];
-    christmastree.candles = [{ name: "Kerze rot", img: "imgs/K1.jpg", price: 0.99 }, { name: "Kerze creme", img: "imgs/K2.jpg", price: 0.99 }, { name: "Lichterkette Kerzenoptik", img: "imgs/K3.jpg", price: 24.99 }, { name: "Lichterkette Baumüberwurf", img: "imgs/K4.jpg", price: 36.99 }];
-    christmastree.lametta = [{ name: "Lametta silber", img: "imgs/L1.jpg", price: 1.49 }, { name: "Lametta gold", img: "imgs/L2.jpg", price: 1.49 }];
-})(christmastree || (christmastree = {}));
+var Christmastree;
+(function (Christmastree) {
+    Christmastree.data = {
+        "trees": [
+            { name: "Nordmanntanne", price: 34.95 },
+            { name: "Rotfichte", price: 29.90 },
+            { name: "Blaufichte", price: 29.90 },
+            { name: "Edeltanne", price: 39.90 }
+        ],
+        "holders": [
+            { name: "Plastic", price: 29.99 },
+            { name: "Wood", price: 49.99 }
+        ],
+        "ornaments": [
+            { name: "Glaskugel rot", price: 1.54 },
+            { name: "Glaskugel grün", price: 1.54 },
+            { name: "Glaskugel creme", price: 1.54 },
+            { name: "Candycane", price: 3.99 },
+            { name: "Glaskugel,rot,Herz", price: 3.99 },
+            { name: "Glaskugel,candycane,grün", price: 3.99 }
+        ],
+        "candles": [
+            { name: "Kerze rot", price: 0.99 },
+            { name: "Kerze creme", price: 0.99 },
+            { name: "Lichterkette Kerzenoptik", price: 24.99 },
+            { name: "Lichterkette Baumüberwurf", price: 36.99 }
+        ],
+        "lametta": [
+            { name: "Lametta silber", price: 1.49 },
+            { name: "Lametta gold", price: 1.49 }
+        ]
+    };
+})(Christmastree || (Christmastree = {}));
 //# sourceMappingURL=Haendler.js.map
--- Aufgabe4/Christmastree/Haendler.ts
+++ Aufgabe4/Christmastree/Haendler.ts
@@ -1,12 +1,45 @@
-namespace christmastree{
+namespace Christmastree{
     
      export interface product{
         name: string;
-        img: string;
         price: number;
      }
-     export let trees: product[] = [{name:"Nordmanntanne", img:"imgs/01.jpg", price: 34.95},{name:"Rotfichte" , img:"imgs/02.jpg",price:29.90},{name:"Blaufichte",img:"imgs/03.jpg",price:29.90},{name:"Edeltanne",img:"imgs/04.jpg",price:39.90} ]
-     export let ornaments: product[] =[{name:"Glaskugel rot", img:"imgs/WK1.jpg",price:1.54},{name:"Glaskugel grün",img:"imgs/WK2.jpg",price:1.54},{name:"Glaskugel creme", img:"imgs/WK3.jpg",price:1.54},{name:"Candycane",img:"imgs/WK4.jpg",price:3.99},{name:"Glaskugel,rot,Herz",img:"imgs/WK5.jpg",price:3.99},{name:"Glaskugel,candycane,grün",img:"imgs/WK6",price:3.99}]
-     export let candles: product[] =[{name:"Kerze rot", img:"imgs/K1.jpg",price:0.99},{name:"Kerze creme", img:"imgs/K2.jpg",price:0.99},{name:"Lichterkette Kerzenoptik",img:"imgs/K3.jpg",price:24.99},{name:"Lichterkette Baumüberwurf", img:"imgs/K4.jpg",price:36.99}]
-     export let lametta: product[] =[{name:"Lametta silber", img:"imgs/L1.jpg", price:1.49},{name:"Lametta gold", img:"imgs/L2.jpg",price:1.49}]
-}
+    
+    export interface products{
+       [key: string]:product[];
+       }
+    
+    export let data : products
+       ={
+             "trees": [
+                {name:"Nordmanntanne", price: 34.95 },
+                {name:"Rotfichte" , price:29.90},
+                {name:"Blaufichte", price:29.90},
+                {name:"Edeltanne", price:39.90}
+               ],
+            "holders": [
+                {name:"Plastic", price:29.99},
+                {name:"Wood", price:49.99}
+               ],
+              "ornaments":[
+                {name:"Glaskugel rot", price:1.54},
+                {name:"Glaskugel grün", price:1.54},
+                {name:"Glaskugel creme", price:1.54},
+                {name:"Candycane", price:3.99},
+                {name:"Glaskugel,rot,Herz", price:3.99},
+                {name:"Glaskugel,candycane,grün", price:3.99}
+                ],
+              "candles":[
+                {name:"Kerze rot", price:0.99},
+                {name:"Kerze creme", price:0.99},
+                {name:"Lichterkette Kerzenoptik", price:24.99},
+                {name:"Lichterkette Baumüberwurf", price:36.99}
+                ],
+              "lametta":[
+                {name:"Lametta silber", price:1.49},
+                {name:"Lametta gold", price:1.49}
+                ]
+              
+            
+    };
+ }
