795b41f3df0e3b311e61b112da720bf85ae64764
verenagirrbach <verenagirrbach@verenas-macbook.eduroam.hs-furtwangen.de>
2018-12-02 16:22:25 +0100

    .

--- Aufgabe6/server.js
+++ Aufgabe6/server.js
@@ -14,10 +14,11 @@ var L06_SendData;
         console.log("Listening");
     }
     function handleRequest(_request, _response) {
-        console.log("I hear voices!");
+        console.log("_request url");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        console.log(_request.url);
         _response.end();
     }
 })(L06_SendData || (L06_SendData = {}));
--- Aufgabe6/server.ts
+++ Aufgabe6/server.ts
@@ -16,13 +16,13 @@ namespace L06_SendData {
     }
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
+        console.log("_request url");
 
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
 
         _response.write(_request.url);
-        
+        console.log(_request.url);
         _response.end();
     }
 }
