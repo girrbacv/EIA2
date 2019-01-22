cded1aacb09d0e3af16be2fe8e28b26a80a9622b
verenagirrbach <verenagirrbach@192.168.0.34>
2019-01-22 18:02:18 +0100

    A11

---
+++ Aufgabe11/DrawObjects.ts
@@ -0,0 +1,18 @@
+namespace rodelbahnA11 {
+
+    export class DrawObjects {
+        xPos: number;
+        yPos: number;
+
+
+        draw(): void {
+
+        };
+
+        move(): void {
+
+        };
+
+    }
+
+}
---
+++ Aufgabe4/Christmastree/Haendler.js
@@ -0,0 +1,34 @@
+var Christmastree;
+(function (Christmastree) {
+    Christmastree.data = {
+        "trees": [
+            { name: "Nordmanntanne", price: 34.95 },
+            { name: "Rotfichte", price: 29.90 },
+            { name: "Blaufichte", price: 29.90 },
+            { name: "Edeltanne", price: 39.90 }
+        ],
+        "holders": [
+            { name: "Plastic", price: 29.99 },
+            { name: "Wood", price: 49.99 }
+        ],
+        "ornaments": [
+            { name: "Glaskugel rot", price: 1.54 },
+            { name: "Glaskugel grün", price: 1.54 },
+            { name: "Glaskugel creme", price: 1.54 },
+            { name: "Candycane", price: 3.99 },
+            { name: "Glaskugel,rot,Herz", price: 3.99 },
+            { name: "Glaskugel,candycane,grün", price: 3.99 }
+        ],
+        "candles": [
+            { name: "Kerze rot", price: 0.99 },
+            { name: "Kerze creme", price: 0.99 },
+            { name: "Lichterkette Kerzenoptik", price: 24.99 },
+            { name: "Lichterkette Baumüberwurf", price: 36.99 }
+        ],
+        "lametta": [
+            { name: "Lametta silber", price: 1.49 },
+            { name: "Lametta gold", price: 1.49 }
+        ]
+    };
+})(Christmastree || (Christmastree = {}));
+//# sourceMappingURL=Haendler.js.map
